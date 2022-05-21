cc786c5db0167c70f1c8ff4461f95ca5b27964a5
jg <57115019+joeyagreco@users.noreply.github.com>
2022-05-20 22:23:32 -0500

    stuff

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from server.service.BotRunner import BotRunner
+from server.util.CustomLogger import CustomLogger
+from server.util.EnvironmentReader import EnvironmentReader
 
 if __name__ == "__main__":
+    MINUTES_IN_BETWEEN_TWEETS = EnvironmentReader.get("MINUTES_IN_BETWEEN_TWEETS", int)
+    LOGGER = CustomLogger.getLogger()
+    LOGGER.info("STARTING BOT...")
     botRunner = BotRunner()
-    botRunner.run()
+    botRunner.run(MINUTES_IN_BETWEEN_TWEETS)
--- server/service/BotRunner.py
+++ server/service/BotRunner.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from server.exception.BusinessSearchTimeoutError import BusinessSearchTimeoutError
 from server.service.YelpReviewService import YelpReviewService
@@ -16,37 +17,41 @@ class BotRunner:
         self.__TMP_DIRECTORY_NAME = "tmp"
         self.__TMP_USER_PROFILE_PIC_FILE_NAME = "tmp_user_profile.jpg"
         self.__TMP_BUSINESS_PIC_FILE_NAME = "tmp_business_profile.jpg"
+        self.__SECONDS_IN_A_MINUTE = 60
 
-    def run(self):
-        try:
-            yelpReviewService = YelpReviewService()
-            review, business = yelpReviewService.getRandomReviewAndBusiness()
-            tmpFolderDirectory = os.path.abspath(
-                os.path.join(os.path.dirname(os.path.realpath(__file__)), f"../{self.__TMP_DIRECTORY_NAME}"))
-            # create the tmp directory if it doesn't already exist
-            if not os.path.exists(tmpFolderDirectory):
-                os.makedirs(tmpFolderDirectory)
-            # delete old images from tmp folder if there are any
-            fullImagePath = os.path.join(tmpFolderDirectory, self.__TMP_USER_PROFILE_PIC_FILE_NAME)
-            if os.path.exists(fullImagePath):
-                os.remove(fullImagePath)
-            fullImagePath = os.path.join(tmpFolderDirectory, self.__TMP_BUSINESS_PIC_FILE_NAME)
-            if os.path.exists(fullImagePath):
-                os.remove(fullImagePath)
-            # will hold images we will send with tweet
-            mediaUrls = list()
-            # download user profile image locally to tmp folder
-            if review.user.imageUrl is not None:
-                ImageDownloader.downloadImageByUrl(review.user.imageUrl, self.__TMP_USER_PROFILE_PIC_FILE_NAME,
-                                                   tmpFolderDirectory)
-                mediaUrls.append(os.path.join(tmpFolderDirectory, self.__TMP_USER_PROFILE_PIC_FILE_NAME))
-            # download business profile image locally to tmp folder
-            if business.imageUrl is not None:
-                ImageDownloader.downloadImageByUrl(business.imageUrl, self.__TMP_BUSINESS_PIC_FILE_NAME,
-                                                   tmpFolderDirectory)
-                mediaUrls.append(os.path.join(tmpFolderDirectory, self.__TMP_BUSINESS_PIC_FILE_NAME))
-            tweetText = f'RATING: {self.__STAR_EMOJI * review.rating}\n\n"{review.text}"\n\nBY: {review.user.name}\n\nBUSINESS: {business.name}\n\n{review.url}'
-            status = TwitterTweeter.createTweet(tweetText, mediaUrls=mediaUrls)
-            self.__LOGGER.info(f"TWEETED SUCCESSFULLY: {EnvironmentReader.get('TWEET_BASE_URL')}{status.id}")
-        except BusinessSearchTimeoutError as e:
-            self.__LOGGER.error(e)
+    def run(self, minutesInBetweenTweets: int):
+        while True:
+            try:
+                yelpReviewService = YelpReviewService()
+                review, business = yelpReviewService.getRandomReviewAndBusiness()
+                tmpFolderDirectory = os.path.abspath(
+                    os.path.join(os.path.dirname(os.path.realpath(__file__)), f"../{self.__TMP_DIRECTORY_NAME}"))
+                # create the tmp directory if it doesn't already exist
+                if not os.path.exists(tmpFolderDirectory):
+                    os.makedirs(tmpFolderDirectory)
+                # delete old images from tmp folder if there are any
+                fullImagePath = os.path.join(tmpFolderDirectory, self.__TMP_USER_PROFILE_PIC_FILE_NAME)
+                if os.path.exists(fullImagePath):
+                    os.remove(fullImagePath)
+                fullImagePath = os.path.join(tmpFolderDirectory, self.__TMP_BUSINESS_PIC_FILE_NAME)
+                if os.path.exists(fullImagePath):
+                    os.remove(fullImagePath)
+                # will hold images we will send with tweet
+                mediaUrls = list()
+                # download user profile image locally to tmp folder
+                if review.user.imageUrl is not None:
+                    ImageDownloader.downloadImageByUrl(review.user.imageUrl, self.__TMP_USER_PROFILE_PIC_FILE_NAME,
+                                                       tmpFolderDirectory)
+                    mediaUrls.append(os.path.join(tmpFolderDirectory, self.__TMP_USER_PROFILE_PIC_FILE_NAME))
+                # download business profile image locally to tmp folder
+                if business.imageUrl is not None:
+                    ImageDownloader.downloadImageByUrl(business.imageUrl, self.__TMP_BUSINESS_PIC_FILE_NAME,
+                                                       tmpFolderDirectory)
+                    mediaUrls.append(os.path.join(tmpFolderDirectory, self.__TMP_BUSINESS_PIC_FILE_NAME))
+                tweetText = f'RATING: {self.__STAR_EMOJI * review.rating}\n\n"{review.text}"\n\nBY: {review.user.name}\n\nBUSINESS: {business.name}\n\n{review.url}'
+                status = TwitterTweeter.createTweet(tweetText, mediaUrls=mediaUrls)
+                self.__LOGGER.info(f"TWEETED SUCCESSFULLY: {EnvironmentReader.get('TWEET_BASE_URL')}{status.id}")
+            except BusinessSearchTimeoutError as e:
+                self.__LOGGER.error(e)
+            self.__LOGGER.info(f"SLEEPING FOR {minutesInBetweenTweets} minutes...")
+            time.sleep(minutesInBetweenTweets * self.__SECONDS_IN_A_MINUTE)
