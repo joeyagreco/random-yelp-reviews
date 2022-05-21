bd7faa39f7f3bdab982070156e88d8cc35e0af4f
jg <57115019+joeyagreco@users.noreply.github.com>
2022-05-21 16:51:54 -0500

    added yelp search terms

--- server/enum/YelpSearchTerm.py
+++ server/enum/YelpSearchTerm.py
@@ -8,9 +8,18 @@ class YelpSearchTerm(Enum):
     """
     Yelp search terms.
     """
+    BAR = "BAR"
+    GROCERY = "GROCERY"
+    GYM = "GYM"
     HIGHEST_REVIEWED = "HIGHEST_REVIEWED"
     LOWEST_REVIEWED = "LOWEST_REVIEWED"
-    
+    MECHANIC = "MECHANIC"
+    RESTAURANT = "RESTAURANT"
+    RETAIL = "RETAIL"
+    HAIRCUT = "HAIRCUT"
+    HOSPITAL = "HOSPITAL"
+    PET = "PET"
+
     @classmethod
     def list(cls):
         return list(map(lambda c: c.value, cls))
